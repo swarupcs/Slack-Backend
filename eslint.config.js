db5971e264a7b4a19c6d18e68c92e607565c809a
Swarup Das <78835981+swarupcs@users.noreply.github.com>
2025-04-23 20:31:02 +0530

    format code with eslint

--- eslint.config.js
+++ eslint.config.js
@@ -1,20 +1,22 @@
-import pluginJs from '@eslint/js';
+import js from '@eslint/js';
+import { defineConfig } from 'eslint/config';
 import simpleImportSort from 'eslint-plugin-simple-import-sort';
 import globals from 'globals';
-/** @type {import('eslint').Linter.Config[]} */
-export default [
+
+export default defineConfig([
   {
+    files: ['**/*.{js,mjs,cjs}'],
     languageOptions: {
       globals: globals.node
     },
     plugins: {
+      js,
       'simple-import-sort': simpleImportSort
     },
     rules: {
       'simple-import-sort/imports': 'error',
       'simple-import-sort/exports': 'error'
-    }
-  },
-
-  pluginJs.configs.recommended
-];
+    },
+    extends: [js.configs.recommended]
+  }
+]);
