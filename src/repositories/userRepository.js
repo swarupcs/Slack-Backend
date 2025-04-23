e0f9e9896ada3b0b48731d8a49112e115cb29106
Swarup Das <78835981+swarupcs@users.noreply.github.com>
2025-04-23 21:54:43 +0530

    Added repository signature

---
+++ src/repositories/userRepository.js
@@ -0,0 +1,13 @@
+export const getUserByEmail = (email) => {};
+
+export const getUserByName = (name) => {};
+
+export const createUser = (user) => {};
+
+export const getUsers = () => {};
+
+export const getUserById = (id) => {};
+
+export const deleteUser = (id) => {};
+
+export const updateUser = (id, user) => {};
