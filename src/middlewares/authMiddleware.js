1a721ad389aab3acc7d3be185421dd0b3606abd0
Swarup Das <78835981+swarupcs@users.noreply.github.com>
2025-04-26 14:28:02 +0530

    Added workspace creation api

--- src/middlewares/authMiddleware.js
+++ src/middlewares/authMiddleware.js
@@ -1,12 +1,12 @@
 import { StatusCodes } from 'http-status-codes';
 import jwt from 'jsonwebtoken';
 
-import { JWT_SECRET } from '../config/serverConfig';
-import userRepository from '../repositories/userRepository';
+import { JWT_SECRET } from '../config/serverConfig.js';
+import userRepository from '../repositories/userRepository.js';
 import {
   customErrorResponse,
   internalErrorResponse
-} from '../utils/common/responseObjects';
+} from '../utils/common/responseObjects.js';
 
 export const isAuthenticated = async (req, res, next) => {
   try {
