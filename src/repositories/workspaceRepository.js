416793ede7cf5fd60426e6f0f29f08942c8250c5
Swarup Das <78835981+swarupcs@users.noreply.github.com>
2025-04-26 13:24:04 +0530

    Added workspace repo

--- src/repositories/workspaceRepository.js
+++ src/repositories/workspaceRepository.js
@@ -3,6 +3,7 @@ import { StatusCodes } from 'http-status-codes';
 import User from '../schema/user.js';
 import Workspace from '../schema/workspace.js';
 import ClientError from '../utils/errors/clientError.js';
+import channelRepository from './channelRepostiory.js';
 import crudRepository from './crudRepository.js';
 const workspaceRepository = {
   ...crudRepository(Workspace),
@@ -21,7 +22,7 @@ const workspaceRepository = {
 
     return workspace;
   },
-  
+
   getWorkspaceByJoinCode: async function (joinCode) {
     const workspace = await Workspace.findOne({
       joinCode
@@ -80,8 +81,45 @@ const workspaceRepository = {
     return workspace;
   },
 
-  addChannelToWorkspace: async function () {},
-  fetchAllWorkspaceByMemberId: async function () {}
+  addChannelToWorkspace: async function (workspaceId, channelName) {
+    const workspace =
+      await Workspace.findById(workspaceId).populate('channels');
+
+    if (!workspace) {
+      throw new ClientError({
+        explanation: 'Invalid data sent from the client',
+        message: 'Workspace not found',
+        statusCode: StatusCodes.NOT_FOUND
+      });
+    }
+
+    const isChannelAlreadyPartOfWorkspace = workspace.channels.find(
+      (channel) => channel.name === channelName
+    );
+
+    if (isChannelAlreadyPartOfWorkspace) {
+      throw new ClientError({
+        explanation: 'Invalid data sent from client',
+        message: 'Channel already part of workspace',
+        statusCode: StatusCodes.FORBIDDEN
+      });
+    }
+
+    const channel = await channelRepository.create({ name: channelName });
+
+    workspace.channels.push(channel);
+    await workspace.save();
+
+    return workspace;
+  },
+  
+  fetchAllWorkspaceByMemberId: async function (memberId) {
+    const workspaces = await Workspace.find({
+      'members.memberId': memberId
+    }).populate('members.memberId', 'username email avatart');
+
+    return workspaces;
+  }
 };
 
 export default workspaceRepository;
