db4599f804d4327933d5668caf1050c4c882054f
Swarup Das <78835981+swarupcs@users.noreply.github.com>
2025-04-26 12:54:57 +0530

    Added workspace repo functions

--- src/repositories/workspaceRepository.js
+++ src/repositories/workspaceRepository.js
@@ -1,10 +1,85 @@
+import { StatusCodes } from 'http-status-codes';
+
+import User from '../schema/user.js';
 import Workspace from '../schema/workspace.js';
+import ClientError from '../utils/errors/clientError.js';
 import crudRepository from './crudRepository.js';
 const workspaceRepository = {
   ...crudRepository(Workspace),
-  getWorkspaceByName: async function () {},
-  getWorkspaceByJoinCode: async function () {},
-  addMemberToWorkspace: async function () {},
+  getWorkspaceByName: async function (workspaceName) {
+    const workspace = await Workspace.findOne({
+      name: workspaceName
+    });
+
+    if (!workspace) {
+      throw new ClientError({
+        explanation: 'Invalid data sent from the client',
+        message: 'Workspace not found',
+        statusCode: StatusCodes.NOT_FOUND
+      });
+    }
+
+    return workspace;
+  },
+  
+  getWorkspaceByJoinCode: async function (joinCode) {
+    const workspace = await Workspace.findOne({
+      joinCode
+    });
+
+    if (!workspace) {
+      throw new ClientError({
+        explanation: 'Invalid data sent from the client',
+        message: 'Workspace not found',
+        statusCode: StatusCodes.NOT_FOUND
+      });
+    }
+
+    return workspace;
+  },
+
+  addMemberToWorkspace: async function (workspaceId, memberId, role) {
+    const workspace = await Workspace.findById(workspaceId);
+
+    if (!workspace) {
+      throw new ClientError({
+        explanation: 'Invalid data sent from the client',
+        message: 'Workspace not found',
+        statusCode: StatusCodes.NOT_FOUND
+      });
+    }
+
+    const isValidUser = await User.findById(memberId);
+    if (!isValidUser) {
+      throw new ClientError({
+        explanation: 'Invalid data sent from the client',
+        message: 'User not found',
+        statusCode: StatusCodes.NOT_FOUND
+      });
+    }
+
+    const isMemberAlreadyPartOfWorkspace = workspace.members.find(
+      (member) => member.memberId == memberId
+    );
+
+    if (isMemberAlreadyPartOfWorkspace) {
+      throw new ClientError({
+        explanation: 'Invalid data sent from the client',
+        message: 'User already part of workspace',
+        statusCode: StatusCodes.FORBIDDEN
+      });
+    }
+
+    workspace.members.push({
+      memberId,
+      role
+    });
+
+    await workspace.save();
+
+    return workspace;
+  },
+
   addChannelToWorkspace: async function () {},
   fetchAllWorkspaceByMemberId: async function () {}
 };
