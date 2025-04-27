c7031c75a19494bb60e7e6c6718d0eb6616e9aa4
Swarup Das <78835981+swarupcs@users.noreply.github.com>
2025-04-27 13:47:33 +0530

    Added redis queue

--- src/services/workspaceService.js
+++ src/services/workspaceService.js
@@ -1,9 +1,11 @@
 import { StatusCodes } from 'http-status-codes';
 import { v4 as uuidv4 } from 'uuid';
 
+import { addEmailtoMailQueue } from '../producers/mailQueueProducer.js';
 import channelRepository from '../repositories/channelRepostiory.js';
 import userRepository from '../repositories/userRepository.js';
 import workspaceRepository from '../repositories/workspaceRepository.js';
+import { workspaceJoinMail } from '../utils/common/mailObject.js';
 import ClientError from '../utils/errors/clientError.js';
 import ValidationError from '../utils/errors/validationError.js';
 
@@ -247,6 +249,10 @@ export const addMemberToWorkspaceService = async (
       memberId,
       role
     );
+    addEmailtoMailQueue({
+      ...workspaceJoinMail(workspace),
+      to: isValidUser.email
+    });
     return response;
   } catch (error) {
     console.log('addMemberToWorkspaceService error', error);
