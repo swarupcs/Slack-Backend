4a9516f8eb2042e0dd67f2f01df602163ba927c8
Swarup Das <78835981+swarupcs@users.noreply.github.com>
2025-04-27 17:35:25 +0530

    Added member api to check subscription of a workspace

--- src/routes/v1/v1Router.js
+++ src/routes/v1/v1Router.js
@@ -1,6 +1,7 @@
 import express from 'express';
 
 import channelRouter from './channel.js';
+import memberRouter from './members.js';
 import userRouter from './users.js';
 import workspaceRouter from './workspaces.js';
 
@@ -11,5 +12,7 @@ router.use('/users', userRouter);
 router.use('/workspaces', workspaceRouter);
 
 router.use('/channels', channelRouter);
+
+router.use('/members', memberRouter);
 
 export default router;
---
+++ src/services/memberService.js
@@ -0,0 +1,22 @@
+import { StatusCodes } from 'http-status-codes';
+
+import userRepository from '../repositories/userRepository.js';
+import workspaceRepository from '../repositories/workspaceRepository.js';
+import ClientError from '../utils/errors/clientError.js';
+import { isUserMemberOfWorkspace } from './workspaceService.js';
+
+export const isMemberPartOfWorkspaceService = async (workspaceId, memberId) => {
+  const workspace = await workspaceRepository.getById(workspaceId);
+
+  const isUserAMember = isUserMemberOfWorkspace(workspace, memberId);
+
+  if (!isUserAMember) {
+    throw new ClientError({
+      explanation: 'User is not a member of the workspace',
+      message: 'User is not a member of the workspace',
+      statusCode: StatusCodes.UNAUTHORIZED
+    });
+  }
+  const user = await userRepository.getById(memberId);
+  return user;
+};
