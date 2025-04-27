8e402334638b78f4d142258de31195714ce400ec
Swarup Das <78835981+swarupcs@users.noreply.github.com>
2025-04-27 17:10:47 +0530

    Added redis board using bull board

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import { StatusCodes } from 'http-status-codes';
 
+import bullServerAdapter from './config/bullBoardConfig.js';
 import connectDB from './config/dbConfig.js';
 import mailer from './config/mailConfig.js';
 import { PORT } from './config/serverConfig.js';
@@ -11,15 +12,13 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.use('/ui', bullServerAdapter.getRouter());
+
 app.use('/api', apiRouter);
 
 app.get('/ping', (req, res) => {
   return res.status(StatusCodes.OK).json({ message: 'pong' });
 });
-
-
-
-
 
 app.listen(PORT, async () => {
   console.log(`Server is running on port ${PORT}`);
@@ -33,6 +32,4 @@ app.listen(PORT, async () => {
       console.log('Server is ready to take our messages');
     }
   });
-
-
 });
