947ddfd19bdf91b82d3e92b64aa045f10f52cba2
Swarup Das <78835981+swarupcs@users.noreply.github.com>
2025-05-10 13:45:28 +0530

    Added presigned logic

--- src/controllers/messageController.js
+++ src/controllers/messageController.js
@@ -1,5 +1,7 @@
 import { StatusCodes } from 'http-status-codes';
 
+import { s3 } from '../config/awsConfig.js';
+import { AWS_BUCKET_NAME } from '../config/serverConfig.js';
 import { getMessagesService } from '../services/messageService.js';
 import {
   customErrorResponse,
@@ -30,5 +32,26 @@ export const getMessages = async (req, res) => {
     return res
       .status(StatusCodes.INTERNAL_SERVER_ERROR)
       .json(internalErrorResponse(error));
+  }
+};
+
+export const getPresignedUrlFromAWS = async (req, res) => {
+  try {
+    const url = await s3.getSignedUrlPromise('putObject', {
+      Bucket: AWS_BUCKET_NAME,
+      Key: `${Date.now()}`,
+      Expires: 60 // 1 minute
+    });
+    return res
+      .status(StatusCodes.OK)
+      .json(successResponse(url, 'Presigned URL generated successfully'));
+  } catch (err) {
+    console.log('Error in getPresignedUrlFromAWS', err);
+    if (err.statusCode) {
+      return res.status(err.statusCode).json(customErrorResponse(err));
+    }
+    return res
+      .status(StatusCodes.INTERNAL_SERVER_ERROR)
+      .json(internalErrorResponse(err));
   }
 };
--- src/routes/v1/messages.js
+++ src/routes/v1/messages.js
@@ -1,10 +1,14 @@
 import express from 'express';
 
-import { getMessages } from '../../controllers/messageController.js';
+import {
+  getMessages,
+  getPresignedUrlFromAWS
+} from '../../controllers/messageController.js';
 import { isAuthenticated } from '../../middlewares/authMiddleware.js';
 
 const router = express.Router();
 
+router.get('/pre-signed-url', isAuthenticated, getPresignedUrlFromAWS);
 router.get('/:channelId', isAuthenticated, getMessages);
 
 export default router;
